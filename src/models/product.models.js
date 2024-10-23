7a174eacb4bec29b36aadb6a7f60caceeae3169a
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-10-23 18:52:47 +0600

    add product model schema

---
+++ src/models/product.models.js
@@ -0,0 +1,43 @@
+import mongoose , {Schema} from "mongoose";
+
+const productSchema = new Schema ({
+    title : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    slug : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    thumbnail : {
+        type : String,
+    },
+    gallery: [
+        {
+            type : String, 
+        }
+    ],
+    categiry : {
+        type : Schema.Types.ObjectId,
+        ref : "Category"
+    },
+    subcategory : {
+         type : Schema.Types.ObjectId,
+        ref : "Subcategory"
+    },
+    inventory : [
+        {
+             type : Schema.Types.ObjectId,
+            ref : "Inventory"
+        }
+    ],
+    discription : {
+        type : String
+    }
+},{
+    timestamps : true
+});
+
+export const Product = mongoose.model.Product?? mongoose.model("Product" , productSchema);
