f6ded96557be8bbe6ce33d565128fd4ac1edf996
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-10-30 21:02:10 +0600

    add  all subcategorys controller and route

--- src/controllers/subcategory.controller.js
+++ src/controllers/subcategory.controller.js
@@ -1,6 +1,5 @@
 import { Category } from "../models/category.models.js";
 import { Subcategory } from "../models/subcategory.modles.js";
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const subCategory = async (req, res) => {
@@ -9,23 +8,34 @@ const subCategory = async (req, res) => {
         if(!(name && category)){
             return res.status(400).json({
                 message : "name and category require",
-            })
-        }
+            });
+        };
         let newSlug ;
 
         if(!slug){
-            newSlug = name.replaceAll(" " ,"-").toLowerCase()
+            newSlug = name.replaceAll(" " ,"-").toLowerCase();
         }else{
-           newSlug = slug.replaceAll(" " ,"-").toLowerCase()
-        }
+           newSlug = slug.replaceAll(" " ,"-").toLowerCase();
+        };
 
         const subCategory = await Subcategory.create({name : name , slug : newSlug , category : category});
 
-        return res.json(new ApiResponse(200 , "subcategory create is database" , subCategory))
+        await Category.updateOne({_id : category},{$push : {subcategory : subCategory._id}});
+
+        return res.json(new ApiResponse(200 , "subcategory create is database" , subCategory));
 
     } catch (error) {
         console.log("subcategory error" , error.message);
-    }
-}
+    };
+};
 
-export {subCategory}
+const allCategory = async (_, res) => {
+    try {
+        const data = await Subcategory.find().populate("category");
+        res.json(new ApiResponse(200 , "all subcategory get done" , data));
+    } catch (error) {
+        console.log("all category error" , error.message);
+    };
+};
+
+export {subCategory , allCategory}
--- src/routes/subcategory.routes.js
+++ src/routes/subcategory.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middleware.js";
 import { adminauth } from "../middlewares/admimauth.muddleware.js";
-import { subCategory } from "../controllers/subcategory.controller.js";
+import { allCategory, subCategory } from "../controllers/subcategory.controller.js";
 
 
 const router = Router()
 
 router.route("/subcategorys/create").post(auth,adminauth,subCategory);
+router.route("/subcategorys/allcategory").get(allCategory);
 
 
 export default router;
